e1a05e7c466052148350c3c45d081efb118aa0f8
Yu Chen <ch.en.yu@outlook.com>
2026-06-15 16:35:11 +0800

    fix .^ -> power in @(x) func

--- matlab/interfaces/prima_matlab/cobyla/cobylb_mod.m
+++ matlab/interfaces/prima_matlab/cobyla/cobylb_mod.m
@@ -364,7 +364,7 @@ classdef cobylb_mod
                     % N.B.: If this happens, do NOT include X into the filter, as F and CONSTR are inaccurate.
                     x(:) = sim(:, n + 1) + d;
                     distsq(n + 1) = sum(fortran.power((x - sim(:, n + 1)), 2), 'all');
-                    distsq(1:n) = reshape(cell2mat(arrayfun(@(j) sum((x - (sim(:, n + 1) + sim(:, j))) .^ 2, 'all'), (1:n), "UniformOutput", false)), [], 1); % Implied do-loop
+                    distsq(1:n) = reshape(cell2mat(arrayfun(@(j) sum(fortran.power((x - (sim(:, n + 1) + sim(:, j))), 2), 'all'), (1:n), "UniformOutput", false)), [], 1); % Implied do-loop
                     %%MATLAB: distsq(1:n) = sum((x - (sim(:,1:n) + sim(:, n+1)))**2, 1)  % Implicit expansion
                     j = fix(fortran.minloc(distsq, 'dim', 1));
                     if distsq(j) <= fortran.power((1.0e-4 * rhoend), 2)
@@ -561,7 +561,7 @@ classdef cobylb_mod
                     % rounding. In an experiment with single precision on 20240317, X = SIM(:, N+1) occurred.
                     x(:) = sim(:, n + 1) + d;
                     distsq(n + 1) = sum(fortran.power((x - sim(:, n + 1)), 2), 'all');
-                    distsq(1:n) = reshape(cell2mat(arrayfun(@(j) sum((x - (sim(:, n + 1) + sim(:, j))) .^ 2, 'all'), (1:n), "UniformOutput", false)), [], 1); % Implied do-loop
+                    distsq(1:n) = reshape(cell2mat(arrayfun(@(j) sum(fortran.power((x - (sim(:, n + 1) + sim(:, j))), 2), 'all'), (1:n), "UniformOutput", false)), [], 1); % Implied do-loop
                     %%MATLAB: distsq(1:n) = sum((x - (sim(:,1:n) + sim(:, n+1)))**2, 1)  % Implicit expansion
                     j = fix(fortran.minloc(distsq, 'dim', 1));
                     if distsq(j) <= fortran.power((1.0e-4 * rhoend), 2)
