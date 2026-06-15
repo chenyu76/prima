bcba97b790c9979e272a2dab75f9c08c1d421743
Yu Chen <ch.en.yu@outlook.com>
2026-06-15 18:35:22 +0800

    fix mex option heap

--- matlab/interfaces/prima_matlab/setup_mex.m
+++ matlab/interfaces/prima_matlab/setup_mex.m
@@ -9,9 +9,20 @@ cd(mex_dir);
 extra_fflags = ['FFLAGS="$FFLAGS', ...
     ' -Wno-missing-include-dirs', ...
     ' -fno-stack-arrays -frecursive', ...
-    ' -fbacktrace -fno-stack-protector', ...
-    ' -ftrampoline-impl=heap', ...  % gfortran >= 14 + libgcc >= 14
-    '"'];
+    ' -fbacktrace -fno-stack-protector'];
+% Conditionally add -ftrampoline-impl=heap (gfortran >= 14 + libgcc >= 14)
+% matching the logic in setup_tools/compile.m
+try
+    compiler_info = mex.getCompilerConfigurations('fortran', 'selected');
+    if contains(lower(compiler_info.Manufacturer), 'gnu')
+        compiler_major = sscanf(compiler_info.Version, '%d');
+        if ~isempty(compiler_major) && compiler_major >= 14
+            extra_fflags = [extra_fflags, ' -ftrampoline-impl=heap'];
+        end
+    end
+catch
+end
+extra_fflags = [extra_fflags, '"'];
 mex('-R2018a', 'FOPTIMFLAGS=-O2', extra_fflags, '../fortran_cos.F90');
 mex('-R2018a', 'FOPTIMFLAGS=-O2', extra_fflags, '../fortran_exp.F90');
 mex('-R2018a', 'FOPTIMFLAGS=-O2', extra_fflags, '../fortran_log.F90');
